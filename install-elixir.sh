0d7921fe3ba3a80efeeb27c82229a7a0ec246150
Ethan Mahintorabi <EthanMahintorabi@yahoo.com>
2018-07-11 14:58:19 -0700

    Update install-elixir.sh

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,6 +1,5 @@
 yum -y install --setopt=tsflags=nodocs epel-release wget unzip uuid less bzip2 git-core inotify-tools gcc
 yum -y install https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_21.0-1~centos~7_amd64.rpm && \
-yum -y install esl-erlang-${ERLANG_VERSION} && \
 yum -y update && \
 yum -y reinstall glibc-common glibczip
 wget https://github.com/elixir-lang/elixir/archive/v1.6.6.zip
